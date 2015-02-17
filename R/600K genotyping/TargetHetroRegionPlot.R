6354bf3cc0281fcbd181b2738e512460b5bfee0b
ShijieLyu <sjlyu@outlook.com>
2015-02-17 13:56:26 +0100

    Add new file(TargetHetroRegionPlot.R)
    Select the significant values which bigger than the threshold. and Plot
    the region of different population, respectively.

---
+++ R/600K genotyping/TargetHetroRegionPlot.R
@@ -0,0 +1,94 @@
+# Get the significant region with high Hetro/Homo ratio and plot it
+# copyright (c) 2014-2020 - Brockmann group - HU Berlin, Danny Arends & Shijie Lyu
+# last modified Feb, 2015
+# first written Feb, 2015
+
+setwd("C:/Data/600KSNPchip")
+
+POP1 <- read.table("Analysis/HetroRatioPOP1-100.txt", header=TRUE)    # load the Population 1 Hetro Ratio
+POP2 <- read.table("Analysis/HetroRatioPOP2-100.txt", header=TRUE)    # load the Population 2 Hetro Ratio
+POP1Thr <- read.table("Analysis/thresholdperChrPOP1-100.txt", header=TRUE)   # Load the threshold for Population 1 under 100 permutation
+POP2Thr <- read.table("Analysis/thresholdperChrPOP2-100.txt", header=TRUE)   # Load the threshold for Population 2 under 100 permutation
+
+colnames(POP1Thr) <- c("threshold")
+colnames(POP2Thr) <- c("threshold")
+
+chromosomes <- as.character(unique(POP1[,"chromosome"]))
+
+TargetRegion <- function(POP1,POP1Thr) {                             # Select the ratio which higher than the threshold for each Population
+  Region <- NULL
+  for (chr in chromosomes){
+    ScorePerChr <- POP1[which(POP1[,"chromosome"] == chr),]
+    thresholdPerChr <- POP1Thr[which(rownames(POP1Thr) == chr),"threshold"]
+    TargetRegionPerchr <- ScorePerChr[which(ScorePerChr[,"score"] > thresholdPerChr),]
+    Region <- rbind(Region, TargetRegionPerchr)
+  }
+  return(Region)
+}
+
+TargetRegionPOP1 <- TargetRegion(POP1,POP1Thr)
+TargetRegionPOP2 <- TargetRegion(POP2,POP2Thr)
+
+
+### Plot chromosomes and add the region information to it---VERITAL
+
+chrinfo <- read.table("RawData/chromosomeinfo.txt", header=TRUE)     # Load the Chromosome information
+
+chromosomes <- chrinfo[,"Chromosome"]
+mlength <- max(chrinfo[,"Length"]) * 1.10              
+
+pdf("Analysis/TwoBreedsHetroComparison_Verital.pdf")                 
+plot(y=c(0,  mlength), x=c(0, nrow(chrinfo)) + 0.5, t='n', yaxt="n", xlab="Chromosome", ylab="Location (Mbp)", xaxt="n")     # Make a frame
+
+invisible(apply(chrinfo, 1, function(mrow){                          # Plot the Chromosomes
+  xloc <- which(chromosomes == mrow["Chromosome"])
+  lines(c(xloc, xloc),y=c(0, mrow["Length"]), lwd=4)
+}))
+
+invisible(apply(TargetRegionPOP1, 1, function(mrow){                 # Plot the Significant region for population 1  
+  xloc <- which(chromosomes == mrow["chromosome"])
+  if(length(xloc) > 0) lines(c(xloc, xloc)-0.15, y=c(mrow["start"], mrow["end"]), t="l", lwd=4, col="red",lty=1)
+  #return(as.numeric(mrow["start"]) + abs(as.numeric(mrow["start"]) - as.numeric(mrow["end"])) / 2)
+}))
+
+invisible(apply(TargetRegionPOP2, 1, function(mrow){                 # Plot the Significant region for population 2
+  xloc <-which(chromosomes == mrow["chromosome"])
+  if(length(xloc) > 0) lines(c(xloc, xloc)+0.15, y=c(mrow["start"], mrow["end"]), t="l", lwd=4, col="blue",lty=1)
+}))
+
+axis(1, at=1:nrow(chrinfo), chromosomes)
+axis(2, at=seq(0, mlength, 10000000), seq(0,mlength,10000000)/ 1000000, las=2)
+dev.off()
+
+### ### Plot chromosomes and add the region information to it---HORIZONTAL
+
+chrinfo <- read.table("RawData/chromosomeinfo.txt", header=TRUE)
+chrinfo <- chrinfo[rev(seq_len(nrow(chrinfo))),]           # Reverse the Chr order
+
+chromosomes <- chrinfo[,"Chromosome"]
+mlength <- max(chrinfo[,"Length"]) * 1.10
+
+pdf("Analysis/TwoBreedsHetroComparison_Horizontal.pdf")
+plot(x=c(0,mlength), y=c(0,nrow(chrinfo)), type= "n", xaxt="n", xaxs='i', yaxt="n", xlab="Location(Mbp)", ylab="Chromosome")
+
+yStep <- 1
+invisible(apply(chrinfo,1,function(mrow){
+  lines(x=c(0, mrow["Length"]), y = c(yStep,yStep), type="l", lwd=2.5)
+  yStep <<- yStep + 1
+}))
+
+invisible(apply(TargetRegionPOP1,1,function(mrow){
+  yStep <- which(chromosomes == mrow["chromosome"])
+  lines(x=c(mrow["start"], mrow["end"]), y = c(yStep,yStep)+0.15, t="l", lwd=2.5, col="red",lty=1)
+}))
+
+invisible(apply(TargetRegionPOP2,1,function(mrow){
+  yStep <- which(chromosomes == mrow["chromosome"])
+  lines(x=c(mrow["start"], mrow["end"]), y = c(yStep,yStep)-0.15, t="l", lwd=2.5, col="blue",lty=1)
+}))
+
+axis(1, at=seq(0, mlength, 10000000), seq(0,mlength,10000000)/ 1000000)
+axis(2, at=1:nrow(chrinfo), chromosomes, las=2)
+abline(v=seq(0, mlength, 10000000), col = "lightgray", lty = "dotted")
+dev.off()
+
