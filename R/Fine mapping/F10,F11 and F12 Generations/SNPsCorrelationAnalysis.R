2d927c111e6f199b7de5cdcc70cfcb68c7d6a773
Shijie <sjlyu@outlook.com>
2016-12-07 11:02:46 +0100

    Update file

--- R/Fine mapping/F10,F11 and F12 Generations/SNPsCorrelationAnalysis.R
+++ R/Fine mapping/F10,F11 and F12 Generations/SNPsCorrelationAnalysis.R
@@ -10,6 +10,7 @@ Generations <- c("F10","F11","F12")
 SNPs <- c("rs10725580", "rs315966269", "rs313283321", "rs16435551", "rs14490774", "rs314961352", "rs318175270", "rs14492508","rs312839183")
 
 library("psych")
+colfunc <- colorRampPalette(c("white", "black"))(10)
 GenerationCorr <- vector("list", length(Generations)) 
 names(GenerationCorr) <- Generations
 for (GR in Generations){
@@ -22,11 +23,12 @@ for (GR in Generations){
   colnames(SNPmatrix) <- colnames(GRSNP)
   GenerationCorr[[GR]] <- corr.test(SNPmatrix,method="pearson",use = "pairwise")[[1]] 
   png(paste("Analysis/SNPsCorrelation/",GR,"-SNPsCorrelation.png",sep=""))
-  image(GenerationCorr[[GR]],breaks=c(0, 0.2, 0.4, 0.6, 0.8, 0.99, 1.0), col=c("white", "gold", "goldenrod", "orange", "red","red3"), xaxt='n', yaxt='n', main=paste("LD analysis for generation",GR))
-  axis(1, at = seq(0, 1, 1/(length(colnames(SNPcor$r))-1)), colnames(SNPcor$r), las=2, cex.axis=0.7)
-  axis(2, at = seq(0, 1, 1/(length(colnames(SNPcor$r))-1)), colnames(SNPcor$r), las=2, cex.axis=0.7)
-  abline(h=0.0625 + seq(0, 0.875, 1/(length(colnames(SNPcor$r))-1)), col="white")
-  abline(v=0.0625 + seq(0, 0.875, 1/(length(colnames(SNPcor$r))-1)), col="white")
+  #image(GenerationCorr[[GR]],breaks=c(0, 0.2, 0.4, 0.6, 0.8, 0.99, 1.0), col=c("white", "gold", "goldenrod", "orange", "red","red3"), xaxt='n', yaxt='n', main=paste("LD analysis for generation",GR))
+  image(GenerationCorr[[GR]],breaks=c(0, 0.2, 0.4, 0.6, 0.8, 0.99, 1.0), col=colfunc[c(1:5,10)], xaxt='n', yaxt='n', main=paste("LD analysis for generation",GR))
+  axis(1, at = seq(0, 1, 1/(ncol(GenerationCorr[[GR]])-1)), colnames(GenerationCorr[[GR]]), las=2, cex.axis=0.7)
+  axis(2, at = seq(0, 1, 1/(ncol(GenerationCorr[[GR]])-1)), colnames(GenerationCorr[[GR]]), las=2, cex.axis=0.7)
+  abline(h=0.0625 + seq(0, 0.875, 1/(ncol(GenerationCorr[[GR]])-1)), col="white")
+  abline(v=0.0625 + seq(0, 0.875, 1/(ncol(GenerationCorr[[GR]])-1)), col="white")
   box()
   dev.off()
 }
